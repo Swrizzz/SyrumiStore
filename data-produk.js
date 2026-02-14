12c3e23c6c297f35da5df0c2c464bbe6cbeeb8ce
Swrizzz <syrza27@gmail.com>
2026-02-15 00:50:21 +0700

    Update data-produk.js

--- data-produk.js
+++ data-produk.js
@@ -43,6 +43,16 @@ const pricelist = {
         { item: '9288 Diamonds', harga: 'Rp2.300.000' }
     ],
     ff: [
-        { item: '5 Diamonds', harga: 'Rp1.000' }
+        { item: 'Membership Mingguan', harga: 'Rp29.000', isPremium: true },
+        { item: 'Membership Bulanan', harga: 'Rp86.000', isPremium: true },
+        { item: '5 Diamonds', harga: 'Rp1.000' },
+        { item: '12 Diamonds', harga: 'Rp1.900' },
+        { item: '50 Diamonds', harga: 'Rp7.000' },
+        { item: '70 Diamonds', harga: 'Rp10.000' },
+        { item: '140 Diamonds', harga: 'Rp19.000' },
+        { item: '355 Diamonds', harga: 'Rp47.000' },
+        { item: '720 Diamonds', harga: 'Rp95.000' },
+        { item: '1440 Diamonds', harga: 'Rp188.000' },
+        { item: '7290 Diamonds', harga: 'Rp945.000' }
     ]
 };
