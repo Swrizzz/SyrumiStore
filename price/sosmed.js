2ad0c1599203f5021bbf371552dcc8cd0183b3b4
Swrizzz <syrza27@gmail.com>
2026-02-15 18:35:49 +0700

    Create sosmed.js

---
+++ price/sosmed.js
@@ -0,0 +1,15 @@
+const pricelistSosmed = {
+    tk_fol: [
+        { item: '100 Followers', harga: 'Rp5.000' },
+        { item: '500 Followers', harga: 'Rp20.000' },
+        { item: '1000 Followers', harga: 'Rp38.000' }
+    ],
+    ig_fol_indo: [
+        { item: '100 Fol Indo', harga: 'Rp10.000' },
+        { item: '500 Fol Indo', harga: 'Rp45.000' }
+    ],
+    ig_fol_mix: [
+        { item: '100 Fol Asing', harga: 'Rp4.000' },
+        { item: '1000 Fol Asing', harga: 'Rp28.000' }
+    ]
+};
