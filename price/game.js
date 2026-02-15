db9cd158edffa44b77cc26c03f1855a4442bddc6
Swrizzz <syrza27@gmail.com>
2026-02-15 18:34:45 +0700

    Create game.js

---
+++ price/game.js
@@ -0,0 +1,35 @@
+const pricelistGame = {
+    ml: [
+        { item: 'Weekly Diamond Pass', harga: 'Rp28.000', isPremium: true },
+        { item: 'Weekly Elite Bundle', harga: 'Rp15.000', isPremium: true },
+        { item: 'Monthly Epic Bundle', harga: 'Rp75.000', isPremium: true },
+        { item: '5 Diamonds', harga: 'Rp1.500' },
+        { item: '12 Diamonds', harga: 'Rp3.500' },
+        { item: '19 Diamonds', harga: 'Rp5.500' },
+        { item: '28 Diamonds', harga: 'Rp8.000' },
+        { item: '44 Diamonds', harga: 'Rp12.000' },
+        { item: '59 Diamonds', harga: 'Rp16.000' },
+        { item: '85 Diamonds', harga: 'Rp23.000' },
+        { item: '170 Diamonds', harga: 'Rp46.000' },
+        { item: '240 Diamonds', harga: 'Rp65.000' },
+        { item: '296 Diamonds', harga: 'Rp80.000' },
+        { item: '408 Diamonds', harga: 'Rp110.000' },
+        { item: '568 Diamonds', harga: 'Rp150.000' },
+        { item: '875 Diamonds', harga: 'Rp230.000' },
+        { item: '2010 Diamonds', harga: 'Rp500.000', isPremium: true },
+        { item: '4830 Diamonds', harga: 'Rp1.200.000', isPremium: true }
+    ],
+    ff: [
+        { item: 'Membership Mingguan', harga: 'Rp29.000', isPremium: true },
+        { item: 'Membership Bulanan', harga: 'Rp86.000', isPremium: true },
+        { item: '5 Diamonds', harga: 'Rp1.000' },
+        { item: '12 Diamonds', harga: 'Rp1.900' },
+        { item: '50 Diamonds', harga: 'Rp7.000' },
+        { item: '70 Diamonds', harga: 'Rp10.000' },
+        { item: '140 Diamonds', harga: 'Rp19.000' },
+        { item: '355 Diamonds', harga: 'Rp47.000' },
+        { item: '720 Diamonds', harga: 'Rp95.000' },
+        { item: '1440 Diamonds', harga: 'Rp188.000' },
+        { item: '7290 Diamonds', harga: 'Rp945.000' }
+    ]
+};
