28353e39a8c1f2ebad0ac4739e25cf36b8f009f4
Swrizzz <syrza27@gmail.com>
2026-02-15 23:41:36 +0700

    Update ppob.js

--- price/ppob.js
+++ price/ppob.js
@@ -26,28 +26,28 @@ const pricelistPPOB = {
         { item: 'Pulsa Indosat 200.000', harga: 'Rp201.500' }
     ],
     xl_axis: [
-        { item: 'Pulsa 5.000', harga: 'Rp7.200' },
-        { item: 'Pulsa 10.000', harga: 'Rp12.100', label: 'Terlaris' },
-        { item: 'Pulsa 15.000', harga: 'Rp16.300' },
-        { item: 'Pulsa 25.000', harga: 'Rp26.200' },
-        { item: 'Pulsa 50.000', harga: 'Rp51.750', label: 'Hemat' },
-        { item: 'Pulsa 100.000', harga: 'Rp101.000' },
-        { item: 'Pulsa 200.000', harga: 'Rp201.000' }
+        { item: 'Pulsa XL 5.000', harga: 'Rp7.200' },
+        { item: 'Pulsa XL 10.000', harga: 'Rp12.100', label: 'Terlaris' },
+        { item: 'Pulsa XL 15.000', harga: 'Rp16.300' },
+        { item: 'Pulsa XL 25.000', harga: 'Rp26.200' },
+        { item: 'Pulsa XL 50.000', harga: 'Rp51.750', label: 'Hemat' },
+        { item: 'Pulsa XL 100.000', harga: 'Rp101.000' },
+        { item: 'Pulsa XL 200.000', harga: 'Rp201.000' }
     ],
     three: [
-        { item: 'Pulsa 5.000', harga: 'Rp7.850' },
-        { item: 'Pulsa 10.000', harga: 'Rp12.850', label: 'Populer' },
-        { item: 'Pulsa 20.000', harga: 'Rp21.750' },
-        { item: 'Pulsa 30.000', harga: 'Rp30.700' },
-        { item: 'Pulsa 50.000', harga: 'Rp51.400' },
-        { item: 'Pulsa 100.000', harga: 'Rp101.400', label: 'Best Value' }
+        { item: 'Pulsa Three 5.000', harga: 'Rp7.850' },
+        { item: 'Pulsa Three 10.000', harga: 'Rp12.850', label: 'Populer' },
+        { item: 'Pulsa Three 20.000', harga: 'Rp21.750' },
+        { item: 'Pulsa Three 30.000', harga: 'Rp30.700' },
+        { item: 'Pulsa Three 50.000', harga: 'Rp51.400' },
+        { item: 'Pulsa Three 100.000', harga: 'Rp101.400', label: 'Best Value' }
     ],
     smartfren: [
-        { item: 'Pulsa 5.000', harga: 'Rp6.300', label: 'Super Murah' },
-        { item: 'Pulsa 10.000', harga: 'Rp11.200', label: 'Populer' },
-        { item: 'Pulsa 20.000', harga: 'Rp21.200' },
-        { item: 'Pulsa 50.000', harga: 'Rp51.200' },
-        { item: 'Pulsa 100.000', harga: 'Rp100.500', label: '👑 Premium Price' }
+        { item: 'Pulsa Smartfren 5.000', harga: 'Rp6.300', label: 'Super Murah' },
+        { item: 'Pulsa Smartfren 10.000', harga: 'Rp11.200', label: 'Populer' },
+        { item: 'Pulsa Smartfren 20.000', harga: 'Rp21.200' },
+        { item: 'Pulsa Smartfren 50.000', harga: 'Rp51.200' },
+        { item: 'Pulsa Smartfren 100.000', harga: 'Rp100.500', label: '👑 Premium Price' }
     ],
     byu: [
         { item: 'Pulsa By.U 5.000', harga: 'Rp6.900' },
