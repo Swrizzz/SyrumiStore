a5e64342188588b19f41d02b7c46c54da4ea91ac
Swrizzz <syrza27@gmail.com>
2026-02-17 00:51:14 +0700

    Create mobile-legends.js

---
+++ price/game/mobile-legends.js
@@ -0,0 +1,22 @@
+const pricelistGame = {
+    ml: [
+        { item: 'Weekly Diamond Pass', harga: 'Rp27.950', isPremium: true },
+        { item: 'Weekly Elite Bundle', harga: 'Rp15.000', isPremium: true },
+        { item: 'Monthly Epic Bundle', harga: 'Rp75.000', isPremium: true },
+        { item: '5 Diamonds', harga: 'Rp1.450' },
+        { item: '12 Diamonds', harga: 'Rp3.400' },
+        { item: '19 Diamonds', harga: 'Rp5.300' },
+        { item: '28 Diamonds', harga: 'Rp7.800' },
+        { item: '44 Diamonds', harga: 'Rp11.600' },
+        { item: '59 Diamonds', harga: 'Rp15.500' },
+        { item: '85 Diamonds', harga: 'Rp22.200' },
+        { item: '170 Diamonds', harga: 'Rp44.400' },
+        { item: '240 Diamonds', harga: 'Rp62.600' },
+        { item: '296 Diamonds', harga: 'Rp77.000' },
+        { item: '408 Diamonds', harga: 'Rp106.000' },
+        { item: '568 Diamonds', harga: 'Rp144.400' },
+        { item: '875 Diamonds', harga: 'Rp221.300' },
+        { item: '2010 Diamonds', harga: 'Rp481.100', isPremium: true },
+        { item: '4830 Diamonds', harga: 'Rp1.155.000', isPremium: true }
+    ]
+};
