fd617aa86f110a4f974da65443a312315408dc13
Swrizzz <syrza27@gmail.com>
2026-02-17 15:51:40 +0700

    Update data-produk.js

--- data-produk.js
+++ data-produk.js
@@ -1,12 +1,12 @@
 const hargaSatuan = {
-    tk_fol: { price: 60, min: 100, max: 1000000 }, 
-    tk_like: { price: 10, min: 100, max: 5000000 }, 
-    tk_view: { price: 0.67, min: 1000, max: 100000000 },
-    ig_fol_indo: { price: 80, min: 100, max: 2000 },
-    ig_fol_mix: { price: 50, min: 100, max: 10000000 },
-    ig_like: { price: 10, min: 100, max: 5000 },
-    ig_view: { price: 0.67, min: 1000, max: 2000000000 },
-    shp_fol: { price: 70, min: 100, max: 2000 }
+    tk_fol: { price: 85, min: 100, max: 1000000 }, 
+    tk_like: { price: 30, min: 100, max: 5000000 }, 
+    tk_view: { price: 1, min: 1000, max: 100000000 },
+    ig_fol_indo: { price: 110, min: 100, max: 2000 },
+    ig_fol_mix: { price: 80, min: 100, max: 10000000 },
+    ig_like: { price: 30, min: 100, max: 5000 },
+    ig_view: { price: 1.5, min: 1000, max: 2000000000 },
+    shp_fol: { price: 100, min: 100, max: 2000 }
 };
 
 const databaseLayanan = {
@@ -21,11 +21,7 @@ const databaseLayanan = {
         { id: 'youtube', name: 'YouTube', icon: 'images/yt.jfif', comingSoon: true },
         { id: 'facebook', name: 'Facebook', icon: 'images/facebook.png', comingSoon: true }
     ],
-    ppob: [
-        { id: 'pulsa', name: 'Pulsa', icon: 'https://cdn-icons-png.flaticon.com/512/3059/3059502.png', label: 'NOMOR HP' },
-        // --- TAMBAHAN MENU E-WALLET DI SINI ---
-        { id: 'sub_ewallet', name: 'Top Up E-Wallet', icon: 'images/wallet.jfif', label: 'PILIH DOMPET' }
-    ],
+    ppob: [{ id: 'pulsa', name: 'Pulsa', icon: 'https://cdn-icons-png.flaticon.com/512/3059/3059502.png', label: 'NOMOR HP' }],
     sosmed_apps: {
         tiktok: [
             { id: 'tk_fol', name: 'TikTok Followers', label: 'USERNAME TIKTOK', isFollowers: true, note: 'Maks 3x24 Jam', pattern: '' },
